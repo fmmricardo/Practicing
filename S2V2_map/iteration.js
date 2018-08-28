b496d443b8e1a924058926fc7804b8a20448047b
fmmricardo <ricardofilipe5@sapo.pt>
2018-08-28 23:49:31 +0100

    map function

--- S2V2_map/iteration.js
+++ S2V2_map/iteration.js
@@ -1,1 +1,23 @@
+//to transform an arrays items with map, we use a callback funtion 
+//to return the  data we want
+
 const strings = ['1','2','3','4','5'];
+const numbers =strings.map(string=> parseInt(string,10));
+
+console.log(numbers);
+
+//challenge
+//Capitalize all the words
+
+const fruits = ['apple', 'pear', 'cherry'];
+
+const capitalizedFruits = fruits.map(fruit => fruit.toUpperCase());
+
+console.log(capitalizedFruits);
+
+//challenge
+// turn the numbers into price strings
+const prices = [5, 4.23, 6.4, 8.09, 3.20];
+
+const displayPrices = price.map(price=>`$${price.toFixed(2)}`);
+console.log(displayPrices);
